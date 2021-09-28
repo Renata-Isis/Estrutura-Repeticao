6aa177cd0c7dbb17b97a36990ffa38fec1a354e9
Renata Rodrigues <renatairodr@gmail.com>
2021-09-28 11:12:44 -0300

    first commit

---
+++ Estrutura de Repetição/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Estrutura_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "Renata", "Tommy", "Diná", "Ionizis" };
+
+
+            /* ForEach*/
+             foreach(string item in palavras)
+            {
+                Console.WriteLine(item);
+            }
+
+            /*For crescente*/
+             for(int contador = 0; contador < palavras.Length; contador++)
+            {
+                Console.WriteLine(palavras[contador]);
+            }
+
+            Console.WriteLine("=============================");
+
+            /*For decrescente*/
+            for (int contador2 = palavras.Length -1; contador2 >= 0; contador2--)
+            {
+                Console.WriteLine(palavras[contador2]);
+            }
+
+        }
+    }
+}
